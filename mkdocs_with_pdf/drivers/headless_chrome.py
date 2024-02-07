f977cfdf346c5cfc6fc14bb0541c113b6ab57904
Dom <waltersdom@gmail.com>
2024-02-07 13:22:02 +0000

    feat: add support for mermaid diagrams in PDF

--- mkdocs_with_pdf/drivers/headless_chrome.py
+++ mkdocs_with_pdf/drivers/headless_chrome.py
@@ -1,8 +1,11 @@
 import os
+import html as html_lib
 from logging import Logger
 from shutil import which
 from subprocess import PIPE, Popen
 from tempfile import NamedTemporaryFile
+import re
+import tempfile
 
 
 class HeadlessChromeDriver(object):
@@ -23,6 +26,37 @@ class HeadlessChromeDriver(object):
     def render(self, html: str) -> str:
         temp = NamedTemporaryFile(delete=False, suffix='.html')
         try:
+            mermaid_regex = r'<pre class="mermaid"><code>(.*?)</code></pre>'
+            mermaid_matches = re.findall(mermaid_regex, html, flags=re.DOTALL)
+            # Add a member variable for the output directory.
+            self.output_dir = "./img_out"
+
+            # Create the output directory if it does not exist.
+            if not os.path.exists(self.output_dir):
+                os.makedirs(self.output_dir)
+            # Convert each Mermaid diagram to an image.
+            for i, mermaid_code in enumerate(mermaid_matches):
+                self._logger.info("Converting diagram.")
+                # Create a temporary file to hold the Mermaid code.
+                with tempfile.NamedTemporaryFile(suffix=".mmd") as mermaid_file:
+                    # Write the Mermaid code to the file.
+                    mermaid_code_unescaped = html_lib.unescape(mermaid_code)
+                    mermaid_file.write(mermaid_code_unescaped.encode("utf-8"))
+                    mermaid_file.flush()
+
+                    # Create a filename for the image.
+                    image_filename = os.path.join(self.output_dir, f"diagram_{i+1}.png")
+
+                    # Convert the Mermaid diagram to an image using mmdc.
+                    command = f"mmdc -i {mermaid_file.name} -o {image_filename} -b transparent -t dark --scale 4"
+
+                    os.system(command)
+
+                    # Replace the Mermaid code with the image in the HTML string.
+                    image_html = f'<img src="file://{os.path.abspath(image_filename)}" alt="Mermaid diagram {i+1}">'
+                    html = html.replace(f'<pre class="mermaid"><code>{mermaid_code}</code></pre>', image_html)
+
+            self._logger.info(html)
             temp.write(html.encode('utf-8'))
             temp.close()
 
